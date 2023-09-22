8c027493ea693d786ba9aa198724086c52690feb
Alexandru Tindeche <alexandru.tindeche@gmail.com>
2023-09-22 12:12:05 +0300

    v1.0.1

--- WeatherObject.py
+++ WeatherObject.py
@@ -1,6 +1,6 @@
 class WeatherObj:
-    def __init__(self, time, wind, temperature, humidity, pressure, visibility, cloudCover):
-        self.time = time
+    def __init__(self, t, wind, temperature, humidity, pressure, visibility, cloudCover):
+        self.t = t
         self.wind = wind
         self.temperature = temperature
         self.humidity = humidity
@@ -10,5 +10,5 @@ class WeatherObj:
         
         
     def __str__(self):
-        return self.time + '\n' + self.wind + '\n' + self.temperature + '\n' + self.humidity + '\n' + self.pressure + '\n' + self.visibility + '\n' + self.cloudCover + '\n'
+        return self.t + '\n' + self.wind + '\n' + self.temperature + '\n' + self.humidity + '\n' + self.pressure + '\n' + self.visibility + '\n' + self.cloudCover + '\n'
         
